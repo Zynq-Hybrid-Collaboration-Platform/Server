cf43fda3dc305c2f5b4914b5f325462c6b70e984
abinjils7 <abinjills0904@gmail.com>
2026-03-11 11:46:07 +0530

    changed some logics in organization domain

--- src/domains/organization/organization.model.ts
+++ src/domains/organization/organization.model.ts
@@ -2,21 +2,38 @@ import mongoose, { Schema, Document } from "mongoose";
 
 export interface IOrganization extends Document {
   name: string;
-  slug: string;
-  ownerId: mongoose.Types.ObjectId;
-  membersCount: number;
-  logo?: string;
+  roles: string[];
+  members: mongoose.Types.ObjectId[];
+  createdAt: Date;
 }
-const organizationSchema = new Schema<IOrganization>(
-  {
-    name: { type: String, required: true, trim: true },
-    slug: { type: String, required: true, unique: true, lowercase: true },
-    ownerId: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    membersCount: { type: Number, default: 1 },
-    logo: String,
+
+const organizationSchema = new Schema<IOrganization>({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    index: true,
   },
-  { timestamps: true },
-);
+
+  roles: {
+    type: [String],
+    default: ["admin", "developer", "designer", "qa", "product-manager"], //the role is dynamic can add from the ui these are the default
+  },
+
+  members: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      index: true,
+    },
+  ],
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 export const Organization = mongoose.model<IOrganization>(
   "Organization",
